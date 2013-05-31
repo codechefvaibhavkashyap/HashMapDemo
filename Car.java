0600c3b27c8acf31bd89327e5c6dbde4650747b7
Codechef Vaibhav Kashyap <vaibhav,kashyap@hotmail.com>
2013-05-31 12:35:32 +0530

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HashMapDemo;
+
+/**
+ *
+ * @author Vaibhav
+ */
+
+public class Car {
+  private String name, company;
+        
+        @Override
+        public boolean equals(Object o) {
+        Car m = (Car) o;
+        return m.company.equals(this.company) && m.name.equals(this.name);
+        }
+
+        @Override
+        public int hashCode() 
+        {
+            return name.hashCode() + company.hashCode();
+        }
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	
+}
+
